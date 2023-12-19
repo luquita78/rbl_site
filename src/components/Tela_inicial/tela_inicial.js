1a83b4762f4b01a16923a54b0e96da3249b4b296
luquita78 <lucasbarbosa.oliveira78@gmail.com>
2023-12-19 20:04:32 -0300

    tela inicial

---
+++ src/components/Tela_inicial/tela_inicial.js
@@ -0,0 +1,10 @@
+import "./tela_inicial.css"
+
+function Tela_inicial () {
+    return(
+        <header className = "Tela_inicial">
+        <img src="/imagens/FUNDO.png" alt="tela de fundo"></img>
+        </header>
+    )
+}
+export default Tela_inicial;
