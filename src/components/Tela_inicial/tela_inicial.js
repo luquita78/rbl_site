ccb9a9a518847ec3009337847f97c737689ae62b
luquita78 <lucasbarbosa.oliveira78@gmail.com>
2023-12-19 20:44:25 -0300

    slogan

--- src/components/Tela_inicial/tela_inicial.js
+++ src/components/Tela_inicial/tela_inicial.js
@@ -2,9 +2,16 @@ import "./tela_inicial.css"
 
 function Tela_inicial () {
     return(
-        <header className = "Tela_inicial">
-        <img src="/imagens/FUNDO.png" alt="tela de fundo"></img>
+        <header id = "#">
+            <div className="slogan">
+                <img src="/imagens/SLOGAN.png" alt="SLOGAN"></img>
+            </div>
+            <div className="tela_inicial">
+             <img src="/imagens/FUNDO.png" alt="tela de fundo"></img>
+            </div>
+            
         </header>
+        
     )
 }
 export default Tela_inicial;
