3e548628a70de113549953c27359e5f248c04169
luquita78 <lucasbarbosa.oliveira78@gmail.com>
2023-12-22 13:35:54 -0300

    logo Menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Tela_inicial from './components/Tela_inicial/tela_inicial';
+import Menu from './components/Menu/menu';
 
 function App() {
   return (
     <div className="App">
-
+      <Menu/>
       <Tela_inicial/> 
     </div>
   );
---
+++ src/components/Menu/menu.js
@@ -0,0 +1,13 @@
+import "./menu.css";
+
+function Menu () {
+    return (
+        <header>
+            <div className="logoMenu">
+                <img src="/imagens/LOGO.png" ></img>
+            </div>
+        </header>
+    );
+};
+
+export default Menu;
